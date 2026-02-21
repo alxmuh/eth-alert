f2b8b1fa95be487871f674bf7379c6de8c112543
alxmuh <31935231+alxmuh@users.noreply.github.com>
2026-02-21 19:08:58 +0000

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,22 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "Serverless function is working 🚀"
-  });
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+    );
+
+    const data = await response.json();
+
+    const ethPrice = data.ethereum.usd;
+
+    res.status(200).json({
+      success: true,
+      eth_usd: ethPrice
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Failed to fetch price"
+    });
+  }
 }
